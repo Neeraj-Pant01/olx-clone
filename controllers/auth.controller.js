7de5f324eb65e6da1d55ab1552da28d702c2c03d
Neeraj-Pant01 <npant923@gmail.com>
2024-08-22 00:09:37 +0530

    server

---
+++ controllers/auth.controller.js
@@ -0,0 +1,49 @@
+const userModel = require("../models/userModel.model");
+
+exports.register = async (req, res, next) => {
+    try {
+        const user = await userModel.findOne({ email: req.body.email })
+
+        if (user) return res.status(400).json({message:"user already exists !"})
+
+
+        const newUser = new userModel(req.body)
+        
+        const savedUSer = await newUser.save();
+
+        const { password, ...others } = savedUSer._doc;
+
+        res.status(200).json(others)
+    } catch (err) {
+        res.status(400).json({message:err.message})
+    }
+}
+
+
+exports.login = async (req, res, next) => {
+    try {
+        const user = await userModel.findOne({ email: req.body.email })
+
+        if (!user) return res.status(400).json({message:"user not exist"})
+
+
+        if (req.body.password !== user.password) return next(createError(403, "wrong credentials"))
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others)
+    } catch (err) {
+        res.status(400).json({message:err.message})
+    }
+}
+
+exports.getAUser = async (req,res,next) =>{
+    try{
+        const user = await userModel.findById(req.params.id)
+        res.status(200).json(user)
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
+}
+
+
